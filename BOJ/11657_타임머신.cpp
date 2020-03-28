bdfedb1a61c617bd7f656f8fafd9913c7fdbf54a
Wuppu <wndud1640@gmail.com>
2020-03-28 21:17:21 +0900

    20200328 study

--- BOJ/11657_타임머신.cpp
+++ BOJ/11657_타임머신.cpp
@@ -20,67 +20,121 @@
  * 그냥 다익스트라 알고리즘으로 할 경우, 무한 루프가 생긴다.(되돌아 가는 것이 최단 거리일 수 있으므로)
  * **/
 
+// #include <iostream>
+// #include <vector>
+// #include <queue>
+// #include <memory.h>
+
+// using namespace std;
+
+// const int INF = 90000000;
+// vector<pair<pair<int, int>, int>> edges;
+// vector<int> ret;
+// int matrix[501][501];
+// int N, M;
+// int A, B, C;
+
+// bool bellman_ford(int start)
+// {
+//     ret[start] = 0;
+//     for (int i = 1; i <= N; i++)
+//     {
+//         for (int j = 0; j < M; j++)
+//         {
+//             int u = edges[j].first.first;
+//             int v = edges[j].first.second;
+//             int cost = edges[j].second;
+
+//             if (ret[u] != INF && ret[u] + cost < ret[v])
+//                 ret[v] = ret[u] + cost;
+//         }
+//     }
+
+//     for (int i = 0; i < M; i++)
+//     {
+//         int u = edges[i].first.first;
+//         int v = edges[i].first.second;
+//         int cost = edges[i].second;
+
+//         if (ret[u] != INF && ret[u] + cost < ret[v])
+//             return false;
+//     }
+//     return true;
+// }
+// int main()
+// {
+//     scanf("%d %d", &N, &M);
+//     edges.resize(M + 1);
+//     ret.resize(N + 1, INF);
+//     for (int i = 0; i < M; i++)
+//     {
+//         scanf("%d %d %d", &A, &B, &C);
+//         edges[i] = {{A, B}, C};
+//     }
+
+//     if (bellman_ford(1))
+//     {
+//         for (int i = 2; i <= N; i++)
+//             if (ret[i] < INF)
+//                 printf("%d\n", ret[i]);
+//             else
+//                 printf("-1\n");
+//     }
+//     else
+//         printf("-1\n");
+//     return 0;
+// }
+
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <memory.h>
 
 using namespace std;
 
-const int INF = 90000000;
-vector<pair<pair<int, int>, int>> edges;
-vector<int> ret;
-int matrix[501][501];
-int N, M;
-int A, B, C;
+const int MAX = 501;
+const int INF = 987654321;
 
-bool bellman_ford(int start)
-{
-    ret[start] = 0;
-    for (int i = 1; i <= N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            int u = edges[j].first.first;
-            int v = edges[j].first.second;
-            int cost = edges[j].second;
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
 
-            if (ret[u] != INF && ret[u] + cost < ret[v])
-                ret[v] = ret[u] + cost;
+    int N, M;
+    vector<pair<int, int>> adj[MAX];
+
+    cin >> N >> M;
+    for (int i = 0; i < M; i++) {
+        int u, v, c;
+        cin >> u >> v >> c;
+        adj[u].push_back({v, c});
+    }
+
+    vector<int> ret(N + 1, INF);
+    ret[1] = 0;
+    bool good = false;
+
+    for (int i = 1; i <= N; i++) {
+        good = false;
+
+        for (int j = 1; j <= N; j++) {
+            for (auto temp : adj[j]) {
+                int v = temp.first;
+                int cost = temp.second;
+
+                if (ret[j] != INF && ret[v] > ret[j] + cost) {
+                    ret[v] = ret[j] + cost;
+                    good = true;
+                }
+            }
         }
     }
 
-    for (int i = 0; i < M; i++)
-    {
-        int u = edges[i].first.first;
-        int v = edges[i].first.second;
-        int cost = edges[i].second;
-
-        if (ret[u] != INF && ret[u] + cost < ret[v])
-            return false;
-    }
-    return true;
-}
-int main()
-{
-    scanf("%d %d", &N, &M);
-    edges.resize(M + 1);
-    ret.resize(N + 1, INF);
-    for (int i = 0; i < M; i++)
-    {
-        scanf("%d %d %d", &A, &B, &C);
-        edges[i] = {{A, B}, C};
-    }
-
-    if (bellman_ford(1))
-    {
-        for (int i = 2; i <= N; i++)
+    if (!good) {
+        for (int i = 2; i <= N; i++) {
             if (ret[i] < INF)
-                printf("%d\n", ret[i]);
-            else
-                printf("-1\n");
+                cout << ret[i] << "\n";
+            else 
+                cout << -1 << "\n";
+        }
     }
-    else
-        printf("-1\n");
+    else cout << -1 << "\n";
     return 0;
 }
--- BOJ/1865_웜홀.cpp
+++ BOJ/1865_웜홀.cpp
@@ -1,81 +1,63 @@
 /**
  * 백준 1865번 - 웜홀
- * 방향성 없음
- * 웜홀은 방향성이 있다.
- * 웜홀은 시간이 거꾸로 간다.(음수 간선 존재)
- * Bellman-ford
- * M, W 에 대해서 모두 실행하고
- * 마지막에 확인할 때, 변화가 있으면 음수 루프가 존재한다는 의미이다.
+ * 
+ * 
  **/
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <cstring>
 
 using namespace std;
 
-vector<pair<pair<int, int>, int>> nodes;
-int t;
-int n, m, w;
-int u, v, cost;
-const int INF = 30000000;
+const int INF = 987654321;
 
-bool bellman(int start)
-{
-    vector<int> ret(n + 1, INF);
-    ret[start] = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 0; j < nodes.size(); j++)
-        {
-            int u = nodes[j].first.first;
-            int v = nodes[j].first.second;
-            int cost = nodes[j].second;
-
-            if (ret[u] != INF && ret[u] + cost < ret[v])
-                ret[v] = ret[u] + cost;
-        }
-    }
-
-    for (int i = 0; i < nodes.size(); i++)
-    {
-        int u = nodes[i].first.first;
-        int v = nodes[i].first.second;
-        int cost = nodes[i].second;
-
-        if (ret[u] != INF && ret[u] + cost < ret[v])
-            return true;
-    }
-    return false;
-}
-
-int main()
-{
-    cin.tie(NULL);
+int main() {
+    cin.tie(nullptr);
     ios::sync_with_stdio(false);
 
+    int t;
     cin >> t;
-    while (t--)
-    {
-        nodes.clear();
+
+    while (t--) {
+        int n, m, w;
+        vector<pair<int, int>> adj[501];
+
         cin >> n >> m >> w;
-        for (int i = 0; i < m; i++)
-        {
-            cin >> u >> v >> cost;
-            nodes.push_back({{u, v}, cost});
-            nodes.push_back({{v, u}, cost});
+        
+        for (int i = 0; i < m; i++) {
+            int u, v, c;
+            cin >> u >> v >> c;
+            adj[u].push_back({v, c});
+            adj[v].push_back({u, c});
         }
 
-        for (int i = 0; i < w; i++)
-        {
-            cin >> u >> v >> cost;
-            nodes.push_back({{u, v}, cost * -1});
+        for (int i = 0; i < w; i++) {
+            int u, v, c;
+            cin >> u >> v >> c;
+            adj[u].push_back({v, -c});
         }
 
-        if (bellman(1))
-            cout << "YES\n";
-        else
-            cout << "NO\n";
+        vector<int> ret(n + 1, INF);
+        bool good = false;
+        ret[1] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            good = false;
+            for (int j = 1; j <= n; j++) {
+                for (pair<int, int> p : adj[j]) {
+                    int v = p.first;
+                    int cost = p.second;
+
+                    if (ret[v] > ret[j] + cost) {
+                        ret[v] = ret[j] + cost;
+                        good = true;
+                    }
+                }
+            }
+        }
+        if (good) cout << "YES\n";
+        else cout << "NO\n";
     }
 
     return 0;
